754f102637a5ddb3f2bb595a480684c28af28272
Jesus Politron <polipop.work@gmail.com>
2022-10-21 14:23:34 -0500

    modified categories fetch data, now works with axios

--- src/components/categoryContainer/index.js
+++ src/components/categoryContainer/index.js
@@ -2,15 +2,27 @@ import React from "react";
 import "./categoryContainer.css";
 import { CategoryElement } from "../categoryElement";
 import { useEffect, useState } from "react";
+import axios from "axios";
+
+const baseURL =
+	"https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.REACT_APP_API_KEY;
 
 const CategoryContainer = () => {
 	const [categories, setCategories] = useState([]);
 
+	/*
 	useEffect(() => {
 		fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.REACT_APP_API_KEY)
 			.then((response) => response.json())
 			.then((data) => setCategories(data.genres));
 	}, []);
+	*/
+
+	useEffect(() => {
+		axios.get(baseURL).then((response) => {
+			setCategories(response.data.genres);
+		});
+	});
 
 	return (
 		<div className="CategoryContainer">
