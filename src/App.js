cacdeb1deecfe8b172573b9b033f8564fa52412f
Jesus Politron <polipop.work@gmail.com>
2022-10-23 14:53:12 -0500

    added header sction with the best movie of the day

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import { Header } from "./components/header";
 import { MovieContainer } from "./components/movieContainer";
 import { CategoryContainer } from "./components/categoryContainer";
 import { Footer } from "./components/footer";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [movies, setmovies] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			"https://api.themoviedb.org/3/trending/movie/day?api_key=" + process.env.REACT_APP_API_KEY
+		)
+			.then((response) => response.json())
+			.then((data) => setmovies(data.results));
+	}, []);
+
 	return (
 		<div className="App">
-			<Header />
+			<Header movie={movies[0]} />
 			<h2>Popular movies of today</h2>
-			<MovieContainer />
+			<MovieContainer movies={movies} />
 			<h2>Most popular by category</h2>
 			<CategoryContainer />
 			<Footer />
--- src/components/movieContainer/index.jsx
+++ src/components/movieContainer/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./movieContainer.css";
 import { MoviePoster } from "../moviePoster";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const MovieContainer = () => {
-	const [movies, setmovies] = useState([]);
+const MovieContainer = ({ movies }) => {
+	// Reminder: do the API calls in the correct section
+	// const [movies, setmovies] = useState([]);
 
-	useEffect(() => {
-		fetch(
-			"https://api.themoviedb.org/3/trending/movie/day?api_key=" + process.env.REACT_APP_API_KEY
-		)
-			.then((response) => response.json())
-			.then((data) => setmovies(data.results));
-	}, []);
+	// useEffect(() => {
+	// 	fetch(
+	// 		"https://api.themoviedb.org/3/trending/movie/day?api_key=" + process.env.REACT_APP_API_KEY
+	// 	)
+	// 		.then((response) => response.json())
+	// 		.then((data) => setmovies(data.results));
+	// }, []);
 
 	return (
 		<div className="MovieContainer">
