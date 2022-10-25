c76a8371f31e79fa30b13406f7ba79a664be1072
Jesus Politron <polipop.work@gmail.com>
2022-10-25 14:31:32 -0500

    fixed infinite loop of API calls in the categhory section

--- src/components/categoryContainer/index.js
+++ src/components/categoryContainer/index.js
@@ -7,8 +7,11 @@ import axios from "axios";
 const baseURL =
 	"https://api.themoviedb.org/3/genre/movie/list?api_key=" + process.env.REACT_APP_API_KEY;
 
+const baseURL2 = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10751`;
+
 const CategoryContainer = () => {
 	const [categories, setCategories] = useState([]);
+	const [moviesByGenre, setMoviesByGenre] = useState();
 
 	/*
 	useEffect(() => {
@@ -22,13 +25,21 @@ const CategoryContainer = () => {
 		axios.get(baseURL).then((response) => {
 			setCategories(response.data.genres);
 		});
-	});
+	}, []);
+
+	useEffect(() => {
+		axios.get(baseURL2).then((response) => {
+			setMoviesByGenre(response.data);
+		});
+	}, []);
 
 	return (
 		<div className="CategoryContainer">
-			{categories.map((categoryTag) => {
-				return <CategoryElement key={categoryTag.id} name={categoryTag.name} />;
-			})}
+			<div className="category-navBar">
+				{categories.map((categoryTag) => {
+					return <CategoryElement key={categoryTag.id} name={categoryTag.name} />;
+				})}
+			</div>
 		</div>
 	);
 };
