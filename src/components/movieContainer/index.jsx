8fb21c91963de879be2681845d3c3f89cb742b35
Jesus Politron <polipop.work@gmail.com>
2022-10-24 20:28:48 -0500

    added conditional rendering to the movie container

--- src/components/movieContainer/index.jsx
+++ src/components/movieContainer/index.jsx
@@ -4,22 +4,24 @@ import { MoviePoster } from "../moviePoster";
 // import { useEffect, useState } from "react";
 
 const MovieContainer = ({ movies }) => {
-	return (
-		<div className="MovieContainer">
-			{movies.map((movie) => {
-				return (
-					<MoviePoster
-						key={movie.id}
-						imgURL={"https://image.tmdb.org/t/p/w300" + movie.poster_path}
-						alt={`${movie.original_title} Cinema Poster`}
-						title={movie.title}
-						rating={movie.vote_average}
-						release_date={movie.release_date}
-					/>
-				);
-			})}
-		</div>
-	);
+	if (movies !== []) {
+		return (
+			<div className="MovieContainer">
+				{movies.map((movie) => {
+					return (
+						<MoviePoster
+							key={movie.id}
+							imgURL={"https://image.tmdb.org/t/p/w300" + movie.poster_path}
+							alt={`${movie.original_title} Cinema Poster`}
+							title={movie.title}
+							rating={movie.vote_average}
+							release_date={movie.release_date}
+						/>
+					);
+				})}
+			</div>
+		);
+	}
 };
 
 export { MovieContainer };
