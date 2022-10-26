6a7ec1f362d3e60e38bdbac6dea78a5abc2d52dc
Jesus Politron <polipop.work@gmail.com>
2022-10-25 21:46:29 -0500

    added mos popular section

--- src/components/categoryContainer/index.js
+++ src/components/categoryContainer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./categoryContainer.css";
 import { CategoryElement } from "../categoryElement";
+import { MoviePoster } from "../moviePoster";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -11,7 +12,8 @@ const baseURL2 = `https://api.themoviedb.org/3/discover/movie?api_key=${process.
 
 const CategoryContainer = () => {
 	const [categories, setCategories] = useState([]);
-	const [moviesByGenre, setMoviesByGenre] = useState();
+	// const [moviesByGenre, setMoviesByGenre] = useState([]);
+	const [moviesWithGenre, setMoviesWithGenre] = useState([]);
 
 	/*
 	useEffect(() => {
@@ -29,19 +31,38 @@ const CategoryContainer = () => {
 
 	useEffect(() => {
 		axios.get(baseURL2).then((response) => {
-			setMoviesByGenre(response.data);
+			// setMoviesByGenre(response.data);
+			setMoviesWithGenre(response.data.results);
 		});
 	}, []);
 
-	return (
-		<div className="CategoryContainer">
-			<div className="category-navBar">
-				{categories.map((categoryTag) => {
-					return <CategoryElement key={categoryTag.id} name={categoryTag.name} />;
-				})}
+	// console.log(categories)
+
+	if (moviesWithGenre !== undefined) {
+		return (
+			<div className="CategoryContainer">
+				<div className="category-navBar">
+					{categories.map((categoryTag) => {
+						return <CategoryElement key={categoryTag.id} name={categoryTag.name} />;
+					})}
+				</div>
+				<div className="category-results">
+					{moviesWithGenre.map((movieItem) => {
+						return (
+							<MoviePoster
+								key={movieItem.id}
+								imgURL={"https://image.tmdb.org/t/p/w300" + movieItem.poster_path}
+								alt={`${movieItem.original_title} Cinema Poster`}
+								title={movieItem.title}
+								rating={movieItem.vote_average}
+								release_date={movieItem.release_date}
+							/>
+						);
+					})}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export { CategoryContainer };
--- src/components/moviePoster/index.js
+++ src/components/moviePoster/index.js
@@ -3,18 +3,16 @@ import "./moviePoster.css";
 
 const MoviePoster = (props) => {
 	return (
-		<>
-			<div className="MoviePoster">
-				<img src={props.imgURL} alt={props.alt} />
-				<div className="moviePoster__description">
-					<h3>{props.title}</h3>
-					<p>
-						Rating: <span>{props.rating.toFixed(2)}</span>/10
-					</p>
-					<p>Release date: {props.release_date}</p>
-				</div>
+		<div className="MoviePoster">
+			<img src={props.imgURL} alt={props.alt} />
+			<div className="moviePoster__description">
+				<h3>{props.title}</h3>
+				<p>
+					Rating: <span>{props.rating.toFixed(2)}</span>/10
+				</p>
+				<p>Release date: {props.release_date}</p>
 			</div>
-		</>
+		</div>
 	);
 };
 
